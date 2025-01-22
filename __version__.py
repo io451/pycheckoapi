ea6eae8d29f53a647479cdd752cdbdd4e9c54c4a
IO <prvtangl@gmail.com>
2025-01-22 13:34:46 +0300

    Delete __version__.py

--- __version__.py
+++
@@ -1,7 +0,0 @@
-__title__ = "pycheckoapi"
-__description__ = "Python libary for checko.ru API functions"
-__url__ = "https://github.com/webcartel-https/pycheckoapi"
-__version__ = "0.1"
-__build__ = "indev"
-__author__ = "IO"
-__author_email__ = "prvtangl@gmail.com"
